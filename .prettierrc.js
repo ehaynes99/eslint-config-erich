1875a9697d0aa34bb18a3de40f33248b5080c64b
Eric Haynes <ehaynes99@gmail.com>
2022-07-31 15:33:40 -0400

    switch prettier config to .js

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'always',
+  jsxBracketSameLine: false,
+  printWidth: 80,
+  quoteProps: 'consistent',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
