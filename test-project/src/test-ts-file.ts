234d394ced7ce2024743fd671b6c269a7c731a45
Eric Haynes <ehaynes99@gmail.com>
2022-08-18 17:37:03 -0400

    Update test project. Obviously needs some work.

--- test-project/src/test-ts-file.ts
+++ test-project/src/test-ts-file.ts
@@ -1,20 +1,21 @@
 export const equals = () => {
-  // const a = 'aaa',
+  //const a = 'aaa',
   //   b = 'bbb'
   // let a,
   //   b
-  // console.log('foo'); console.log('bar');
+  console.log('foo')
+  console.log('bar')
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const blah = function blah() {
     console.log('woot')
-  };
-  console.log('***** blah:', blah);
+  }
+  console.log('***** blah:', blah)
   // const foo = 'oof'; console.log('foo');
 
-  if (Date.now() > 0) console.log('hello!');
+  if (Date.now() > 0) console.log('hello!')
 
   while (true) {
-    if (Date.now() % 2) break;
+    if (Date.now() % 2) break
   }
-};
+}
