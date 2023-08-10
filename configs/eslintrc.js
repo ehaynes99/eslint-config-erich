4d515e01095fa2787c9b4fabbcd42ae7b3e48dd1
Eric Haynes <ehaynes99@gmail.com>
2023-08-10 19:23:52 -0400

    Disable no-explicit-any

--- configs/eslintrc.js
+++ configs/eslintrc.js
@@ -96,7 +96,7 @@ module.exports = {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/no-empty-function': 'off',
-        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-floating-promises': 'error',
         '@typescript-eslint/no-for-in-array': 'error',
         ...{
